fe80f8ae1b3bd4fd1a458137eb723dfe18d7f905
Sam Strasser <sam@samstrasser.com>
2023-03-22 20:12:13 -0400

    more idiomatic folders

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const api = require("./pages/api/apiRoutes.js");
+const api = require("./routes/api.js");
 app.use("/api", api);
 
 app.get("/", (req, res) => res.redirect('/api/pods'));
