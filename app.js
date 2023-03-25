f2d71585e64f4dcb89bc2e03099c1720692c9216
Sam Strasser <sam@samstrasser.com>
2023-03-25 14:51:42 -0400

    rename api to pods

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const api = require("./routes/api.js");
-app.use("/api", api);
+const pods = require("./routes/pods.js");
+app.use("/pods", pods);
 
-app.get("/", (req, res) => res.redirect('/api/pods'));
+app.get("/", (req, res) => {
+    res.type('html').send(`
+        <html><body>
+            <h1>Try:</h1>
+            <ol>
+                <li>/pods</li>
+            </ol>
+        </body></html>
+    `);
+});
 
 app.listen(port, () => console.log(`Started app on port ${port}`));
